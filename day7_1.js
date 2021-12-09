65eba3ea977f077817315098edcc33a2193f1875
jimc66 <jim@jimcuff.com>
2021-12-09 12:01:12 -0500

    day 7 1 file for challenge 1 and 2

--- day7_1.js
+++ day7_1.js
@@ -28,6 +28,49 @@ function calc_fuel (crab_array, rally_point ) {
 }
 
 
+// crab_move_cost
+//
+// input - spaces to move (int)
+// return value - the cost (int)
+//
+function crab_move_cost (spaces) {
+    total_cost = 0;
+    incremental_cost = 1;
+    for (var i =0; i < spaces; i++){
+        total_cost = total_cost + incremental_cost;
+        incremental_cost = incremental_cost + 1;
+    }
+return (total_cost);
+}
+
+//
+//   calc_fuel2
+//
+//  For the day 2 calculation  
+//
+//function to calc the fuel used to get the crabs to a specific position
+// 
+// takes as input: 
+//   array of crabs (their spot)
+//   where you want them to end up
+//
+//
+// returns an integer which is the SUM of all the "fuel" used to get them there
+function calc_fuel2 (crab_array, rally_point ) {
+    items_in_array = crab_array.length;
+    // figure out which 5 lines to read
+    // 
+    sum_fuel = 0;
+    for (var j = 0; j < items_in_array; j++) {
+        math_space_to_move = crab_array[j] - rally_point;
+        space_to_move = Math.abs(math_space_to_move);
+        one_crab_cost = crab_move_cost (space_to_move);
+        sum_fuel = sum_fuel + one_crab_cost;
+            
+        }
+  
+    return (sum_fuel);
+}
 
 var crab_array_string = fs.readFileSync (file_name).toString().split("\n");
 
@@ -47,10 +90,12 @@ low_crab = Math.min(...crab_nums);
 // that is, no negative numbers in the data set
 var total_fuel_by_pos = Array(high_crab).fill(-1);
 number_of_crabs = crab_nums.length;
+
+console.log ('Day 1 challenge answers: \n');
 lowest_fuel = -1;
 magic_pos = -1;
-for (var i = 0; i < number_of_crabs; i++){ // is this "off by one" on the high end? check it
-    fuel_used = calc_fuel (crab_nums, crab_nums[i]);
+for (var i = 0; i < high_crab; i++){ // is this "off by one" on the high end? check it
+    fuel_used = calc_fuel (crab_nums, i);
     if (lowest_fuel == -1) { // first one gets set to start
         lowest_fuel = fuel_used;
         magic_pos = i;
@@ -63,4 +108,26 @@ for (var i = 0; i < number_of_crabs; i++){ // is this "off by one" on the high e
     }
 }
 console.log ('low fuel  %i', lowest_fuel);
-console.log ('crab spot %i', crab_nums[magic_pos]);
+console.log ('crab spot %i', magic_pos);
+// 
+// Now - let's do day 2
+// repeat code block (sorry) except for function call
+
+console.log ('\n============\nDay 2 challenge answers: \n');
+lowest_fuel = -1;
+magic_pos = -1;
+for (var i = 0; i < high_crab; i++){ // is this "off by one" on the high end? check it 
+    fuel_used = calc_fuel2 (crab_nums, i); // not crab_nums[i], just i !!!
+    if (lowest_fuel == -1) { // first one gets set to start
+        lowest_fuel = fuel_used;
+        magic_pos = i;
+    }
+    else { // it's not the first one
+        if (fuel_used < lowest_fuel) {
+            lowest_fuel = fuel_used;
+            magic_pos = i;
+        }
+    }
+}
+console.log ('low fuel  %i', lowest_fuel);
+console.log ('crab spot %i', magic_pos);
